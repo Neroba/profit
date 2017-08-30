a83be12b98dd0d5e9f319a1c1693bde7fbcab590
Uri <tais@ua.fm>
2017-08-30 12:47:16 +0300

    Родительская модель

---
+++ app/Model.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App;
+
+/**
+ * Description of Model
+ *
+ * @author Администратор
+ */
+class Model {
+    const TABLE = ''; 
+
+    public static function findAll(){
+        $db = new \App\Db();
+        return $db->query(
+                'SELECT * FROM '.static::TABLE, 
+                static::class
+                );
+    }
+    //put your code here
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,9 @@
 
 namespace App\Models;
 
-class User {
+class User extends \App\Model{
+    const TABLE = 'users';
     public $email;
     public $name;
-    
-    public static function findAll(){
-        $db = new \App\Db();
-        return $db->query(
-                'SELECT * FROM users', 
-                'App\Models\User'
-                );
     }
-}
+
